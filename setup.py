3825950b0c9c7358fdb057d2cd470c82b7d284bb
ben <b.talbot@qut.edu.au>
2021-03-03 13:23:14 +1000

    Rename from wrappers-ros to acrv-ros-wrappers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=['wrappers_ros'],
+d = generate_distutils_setup(packages=['acrv_ros_wrappers'],
                              package_dir={'': 'src'})
 
 setup(**d)
