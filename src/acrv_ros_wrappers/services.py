7ccb2ae62822e4b5e97224b2db5ce1e7f4b50e9e
Ben Talbot <b.talbot@qut.edu.au>
2021-05-04 08:53:54 +1000

    Improve transparency of service name failures

--- src/acrv_ros_wrappers/services.py
+++ src/acrv_ros_wrappers/services.py
@@ -8,7 +8,8 @@ def load_service(name, *args, **kwargs):
     service_str = next((p for n, p in _services.items() if n == name), None)
     if not service_str:
         raise ValueError("Could not find a '%s' service to load. Please "
-                         "confirm the name." % name)
+                         "confirm the name. Valid names are:\n\t%s" %
+                         (name, ",".join(service_list())))
     return getattr(
         importlib.import_module('.refinenet', package='acrv_ros_wrappers'),
         'Refinenet')(*args, **kwargs)
