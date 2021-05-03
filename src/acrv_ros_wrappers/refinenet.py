84fd487bd56780ffb255aacce689f6f4503d0eea
Ben Talbot <b.talbot@qut.edu.au>
2021-05-04 09:11:13 +1000

    Link in RefineNet base class

--- src/acrv_ros_wrappers/refinenet.py
+++ src/acrv_ros_wrappers/refinenet.py
@@ -1,6 +1,11 @@
+from refinenet import RefineNet as RefineNetBase
+
 from .service import Service
 
 
 class Refinenet(Service):
+
     def __init__(self):
-        super(Refinenet, self).__init__('refinenet')
+        super(Refinenet, self).__init__('refinenet', None)
+
+        self.base = RefineNetBase()
--- src/acrv_ros_wrappers/service.py
+++ src/acrv_ros_wrappers/service.py
@@ -1,3 +1,8 @@
+import rospy
+
+
 class Service:
-    def __init__(self, name):
+
+    def __init__(self, name, service_class):
         self.name = name
+        self.service = rospy.ServiceProxy(name, service_class)
