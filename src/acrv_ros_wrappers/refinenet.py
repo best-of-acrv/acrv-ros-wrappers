413ebedd387cb837d0810e11e41a31bcd874601d
Ben Talbot <b.talbot@qut.edu.au>
2021-05-05 13:32:13 +1000

    Use VOC by default for RefineNet

--- src/acrv_ros_wrappers/refinenet.py
+++ src/acrv_ros_wrappers/refinenet.py
@@ -11,11 +11,11 @@ class RefineNet(Service):
 
     def __init__(self):
         super(RefineNet, self).__init__('refinenet', RefineNetService)
-        self.base = RefineNetBase(load_pretrained='nyu')
+        self.base = RefineNetBase(load_pretrained='voc', num_classes=21)
 
     def callback(self, req):
-        return RefineNetService._response_class(
-            segmented_image=msgify(Image,
-                                   self.base.predict(
-                                       image=numpify(req.rgb_image)),
-                                   encoding=req.rgb_image.encoding))
+        return RefineNetService._response_class(segmented_image=msgify(
+            Image,
+            self.base.predict(image=numpify(req.rgb_image),
+                              colour_map_preset='voc'),
+            encoding=req.rgb_image.encoding))
